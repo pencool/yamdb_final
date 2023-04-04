d42dd44af019353efc7b092cba336caa04fdc63a
Павел Корчилов <pencool2@yandex.ru>
2023-04-04 21:06:14 +0300

    workflow 2nd review

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-# from django.views.generic import TemplateView
+from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -9,11 +9,11 @@ from rest_framework import permissions
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-    # path(
-    #     'redoc/',
-    #     TemplateView.as_view(template_name='redoc.html'),
-    #     name='redoc'
-    # ),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
 
 schema_view = get_schema_view(
@@ -33,6 +33,6 @@ urlpatterns += [
         schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'),
+    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+    #     name='schema-redoc'),
 ]
